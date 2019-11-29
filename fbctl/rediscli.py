3b59b4b507a594f372684478bc19cd0311df3f1d
soonbee <maru204@gmail.com>
2019-11-29 15:26:41 +0900

    Update cli config get

--- fbctl/rediscli.py
+++ fbctl/rediscli.py
@@ -89,7 +89,22 @@ class RedisCliConfig(object):
         if not isinstance(all, bool):
             logger.error("option '--all' can use only 'True' or 'False'")
             return
-        sub_cmd = 'config get "{key}"'.format(key=key)
+        if (not host or not port) and not all:
+            logger.error("Enter host and port or use '--all' option.")
+            return
+        sub_cmd = 'config get "{key}" 2>&1'.format(key=key)
+        if all:
+            meta = []
+            ret = RedisCliUtil.command_all_async(sub_cmd)
+            for m_s, host, port, result, message in ret:
+                addr = '{}:{}'.format(host, port)
+                if result == 'OK':
+                    _, value = message.split('\n')
+                    meta.append([m_s, addr, value])
+                else:
+                    meta.append([m_s, addr, color.red(result)])
+            utils.print_table([['TYPE', 'ADDR', 'RESULT']] + meta)
+            return
         if all:
             RedisCliUtil.command_all(
                 sub_cmd=sub_cmd,
